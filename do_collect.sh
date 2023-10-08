fc7770cedf4866ea5a13d15d8c89c4a0efc8a56a
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-08 21:51:55 +0200

    100 -> 1000

--- do_collect.sh
+++ do_collect.sh
@@ -2,7 +2,7 @@
 
 python3 ../cdblib/cdbbulkpv.py -c 32 --stable --user rob uho.epd >uho_cdbpv.epd
 python3 ../caissatrack/caissatrack.py uho_cdbpv.epd >>uhotrack.csv
-python3 ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 100 --ignore2folds >uho_daily_shortest.epd
+python3 ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 1000 --ignore2folds >uho_daily_shortest.epd
 
 git add uho_cdbpv.epd uhotrack.csv uho_daily_shortest.epd
 git diff --staged --quiet || git commit -m "update data"
