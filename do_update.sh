1869d121576459df8b5d820b7d09c746a3c720bc
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-08 21:10:11 +0200

    reformat shell scripts

--- do_update.sh
+++ do_update.sh
@@ -2,6 +2,6 @@
 
 python3 ../caissatrack/plotdata.py uhotrack.csv --logplot --cutOff 200 --negplot --logplot --edgeMin 85 --edgeMax 105
 
-git add uhotrack.png uhotrackpv.png uhotracktime.png 
+git add uhotrack.png uhotrackpv.png uhotracktime.png
 git diff --staged --quiet || git commit -m "update plots"
-git push origin main >& push.log
+git push origin main >&push.log
