91182b131daa5ee98a53c586675b60fb2f12499a
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-16 07:40:52 +0200

    64 -> 48

--- do_collect.sh
+++ do_collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../cdblib/cdbbulkpv.py -c 64 --stable --user rob uho.epd >uho_cdbpv.epd
+python3 ../cdblib/cdbbulkpv.py -c 48 --stable --user rob uho.epd >uho_cdbpv.epd
 python3 ../caissatrack/caissatrack.py uho_cdbpv.epd >>uhotrack.csv
 python3 ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 1000 --ignore2folds >uho_daily_shortest.epd
 
