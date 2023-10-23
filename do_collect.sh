9ffc7aebab7700ff2e1b1bc88b3190960b7dea82
Robert Nurnberg <robert.nurnberg@gmx.de>
2023-10-23 18:41:26 +0200

    push

--- do_collect.sh
+++ do_collect.sh
@@ -4,6 +4,6 @@ python3 ../cdblib/cdbbulkpv.py -c 16 --stable --user rob uho.epd >uho_cdbpv.epd
 python3 ../caissatrack/caissatrack.py uho_cdbpv.epd >>uhotrack.csv
 python3 ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 1000 --ignore2folds >uho_daily_shortest.epd
 
-git add uho_cdbpv.epd uhotrack.csv uho_daily_shortest.epd
+git add uhotrack.csv uho_daily_shortest.epd
 git diff --staged --quiet || git commit -m "update data"
-git push origin main >&push.log
+git push >&push.log
