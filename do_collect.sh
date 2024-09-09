1176eb7fd7e5c3d56adb85105a4b7820d2f2a311
Robert Nurnberg <robert.nurnberg@gmx.de>
2024-09-09 22:21:39 +0200

    drop -b

--- do_collect.sh
+++ do_collect.sh
@@ -10,12 +10,12 @@ if [ -f "$temp_file" ]; then
     exit 0
 fi
 
-python3 ../cdblib/cdbbulkpv.py -c 24 -b 100000 --stable --user rob uho.epd >"$temp_file"
+python ../cdblib/cdbbulkpv.py -c 24 --stable --user rob uho.epd >"$temp_file"
 
 mv "$temp_file" uho_cdbpv.epd
 
-python3 ../caissatrack/caissatrack.py uho_cdbpv.epd >>uhotrack.csv
-python3 ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 1000 --ignore2folds >uho_daily_shortest.epd
+python ../caissatrack/caissatrack.py uho_cdbpv.epd >>uhotrack.csv
+python ../caissatrack/extract_fens.py uho_cdbpv.epd --shortest 1000 --ignore2folds >uho_daily_shortest.epd
 
 git add uhotrack.csv uho_daily_shortest.epd
 git diff --staged --quiet || git commit -m "update data"
