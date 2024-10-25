04f9e9c21506aebc0e1950ca5572c22f7655a5d4
Robert Nurnberg <robert.nurnberg@gmx.de>
2024-10-25 13:50:36 +0200

    fix

--- do_update.sh
+++ do_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 ../caissatrack/plotdata.py uhotrack.csv --logplot --cutOff 200 --negplot --logplot --edgeMin 90 --edgeMax 105 --PvLengthPlot pvlength.png
+python3 ../caissatrack/plotdata.py uhotrack.csv --cutOff 200 --negplot --logplot --edgeMin 90 --edgeMax 105 --PvLengthPlot pvlength.png
 
 git add uhotrack.png uhotrackpv.png uhotracktime.png
 git diff --staged --quiet || git commit -m "update plots"
