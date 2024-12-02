64b0325d419a62e7db9ff03a1841c341ab027fba
Robert Nürnberg <robert.nurnberg@gmx.de>
2024-12-02 09:32:45 +0100

    add progress plot over last 100 data points (#6)

--- do_update.sh
+++ do_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python3 ../caissatrack/plotdata.py uhotrack.csv --cutOff 200 --negplot -ll --edgeMin 90 --edgeMax 105 --PvLengthPlot pvlength.png
+python ../caissatrack/plotdata.py uhotrack.csv --cutOff 200 --negplot -ll --edgeMin 90 --edgeMax 105 --PvLengthPlot pvlength.png
 
-git add uhotrack.png uhotrackpv.png uhotracktime.png
+git add uhotrack.png uhotrackpv.png uhotracktime.png uhotracktime-100.png
 git diff --staged --quiet || git commit -m "update plots"
 git push origin main >&push.log
